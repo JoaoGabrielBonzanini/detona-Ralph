ae38cf3e1abbecb1b04976705ba159e4a3c93751
JoaoGabrielBonzanini <joaogabrielbonzanini@gmail.com>
2023-12-05 10:20:20 -0300

    audio plays when hits the box

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,19 +1,41 @@
 const state = {
-    view:{
+    view: {
         squares: document.querySelectorAll(".square"),
         enemy: document.querySelector(".enemy"),
         timeLeft: document.querySelector("#time-left"),
         score: document.querySelector("#score")
     },
-    values:{
-        timerId: null,
+    values: {
         gameVelocity: 1000,
         hitPosition: 0,
         result: 0,
+        currentTime: 60,
+    },
+    actions: {
+        // timerId: null,
+        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
     }
 };
 
-function randomSquare(){
+function playSound(audioName) {
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.1;
+    audio.play();
+}
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if (state.values.currentTime <= 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId);
+        alert("Game Over! Score: " + state.values.result)
+    }
+}
+
+function randomSquare() {
     state.view.squares.forEach((square) => {
         square.classList.remove("enemy");
     })
@@ -24,24 +46,25 @@ function randomSquare(){
     state.values.hitPosition = randomSquare.id;
 }
 
-function moveEnemy() {
-    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
-}
+// function moveEnemy() {
+//     state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
+// }
 
-function addListenerHitBox(){
+function addListenerHitBox() {
     state.view.squares.forEach((square) => {
         square.addEventListener("mousedown", () => {
-            if(square.id === state.values.hitPosition){
+            if (square.id === state.values.hitPosition) {
                 state.values.result++
                 state.view.score.textContent = state.values.result;
                 state.values.hitPosition = null;
+                playSound("hit");
             }
         })
     })
 }
 
-function initialize(){
-    moveEnemy();
+function initialize() {
+    // moveEnemy();
     addListenerHitBox();
 }
 
